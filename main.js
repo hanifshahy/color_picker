11b82e12d5b2ac8f0edf7c8e83e1e8de594a914f
hanifshahy <hanifshahy@gmail.com>
2024-06-29 00:39:44 +0430

    Perform separation of concerns
    All styles moved to style.css and methods and objects moved to main.js for a cleaner code.

---
+++ main.js
@@ -0,0 +1,31 @@
+
+var rgb = {
+  redSet() {
+    let rgbRedOutputNumber = document.getElementById('rgb-red-output-number');
+    let rgbRedInputRange = document.getElementById('rgb-red-input-range');
+    rgbRedOutputNumber.innerHTML = rgbRedInputRange.value;
+    rgb.colorSet();
+  }
+  ,
+  greenSet() {
+    let rgbGreenOutputNumber = document.getElementById('rgb-green-output-number');
+    let rgbGreenInputRange = document.getElementById('rgb-green-input-range');
+    rgbGreenOutputNumber.innerHTML = rgbGreenInputRange.value;
+    rgb.colorSet();
+  }
+  ,
+  blueSet() {
+    let rgbBlueOutputNumber = document.getElementById('rgb-blue-output-number');
+    let rgbBlueInputRange = document.getElementById('rgb-blue-input-range');
+    rgbBlueOutputNumber.innerHTML = rgbBlueInputRange.value;
+    rgb.colorSet();
+  }
+  ,
+  colorSet() {
+    let rbgOutputColor = document.getElementById('rgb-output-color');
+    let red = document.getElementById('rgb-red-input-range').value;
+    let green = document.getElementById('rgb-green-input-range').value;
+    let blue = document.getElementById('rgb-blue-input-range').value;
+    rbgOutputColor.style.backgroundColor = `rgb(${red},${green},${blue})`;
+  }
+}
