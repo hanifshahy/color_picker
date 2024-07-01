dceec957c63192b02df4babafe58aa761565acca
hanifshahy <hanifshahy@gmail.com>
2024-07-01 13:42:38 +0430

    Add index.html, style.css and main.js
    Our app will support opacity changes

--- main.js
+++ main.js
@@ -21,11 +21,40 @@ let rgb = {
     rgb.colorSet();
   }
   ,
-  colorSet(alpha=1) {
+  alphaSet() {
+    let rgbAlphaOutputNumber = document.getElementById('rgb-alpha-output-number');
+    let rgbAlphaInputRange = document.getElementById('rgb-alpha-input-range');
+    let alpha = ((rgbAlphaInputRange.value) / 100).toPrecision(2);
+    rgbAlphaOutputNumber.innerHTML = alpha;
+    this.colorSet(alpha)
+  }
+  ,
+  colorSet(alphaParam = 1) {
     let rbgOutputColor = document.getElementById('rgb-output-color');
     let red = document.getElementById('rgb-red-input-range').value;
     let green = document.getElementById('rgb-green-input-range').value;
     let blue = document.getElementById('rgb-blue-input-range').value;
-    rbgOutputColor.style.backgroundColor = `rgb(${red},${green},${blue},${alpha})`;
+    let alpha= document.getElementById('rgb-alpha-input-range');
+    alpha.value=alphaParam*100;
+    rbgOutputColor.style.backgroundColor = `rgb(${red},${green},${blue},${alphaParam})`;
+  }
+  ,
+  prepareRgb() {
+    let rgbSelection = document.getElementById('rgb-selection');
+    let rgbAlphaArea = document.getElementById('rgb-alpha-area');
+    if (!rgbSelection.checked) { return; };
+    // alert(rgbSelection.checked);
+    rgbSelection.checked=true;
+    rgbAlphaArea.style = 'display: none;';
+    this.colorSet();
+    document.getElementById('rgb-alpha-output-number').innerHTML='1.0';
+  }
+  ,
+  prepareRgba() {
+    let rgbaSelection = document.getElementById('rgba-selection');
+    let rgbAlphaArea = document.getElementById('rgb-alpha-area');
+    if (!rgbaSelection.checked) { return; }
+    rgbaSelection.checked=true;
+    rgbAlphaArea.style = 'display: inline-block;';
   }
 }
